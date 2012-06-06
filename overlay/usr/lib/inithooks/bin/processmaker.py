2f71efa1495851a9f86a4f2e051a7a09d1346702
Alon Swartz <alon@turnkeylinux.org>
2012-06-06 13:54:04 +0300

    inithook to set admin password and email

---
+++ overlay/usr/lib/inithooks/bin/processmaker.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+"""Set ProcessMaker admin password and email
+
+Option:
+    --pass=     unless provided, will ask interactively
+    --email=    unless provided, will ask interactively
+
+"""
+
+import sys
+import getopt
+import hashlib
+
+from dialog_wrapper import Dialog
+from mysqlconf import MySQL
+
+def usage(s=None):
+    if s:
+        print >> sys.stderr, "Error:", s
+    print >> sys.stderr, "Syntax: %s [options]" % sys.argv[0]
+    print >> sys.stderr, __doc__
+    sys.exit(1)
+
+def main():
+    try:
+        opts, args = getopt.gnu_getopt(sys.argv[1:], "h",
+                                       ['help', 'pass=', 'email='])
+    except getopt.GetoptError, e:
+        usage(e)
+
+    password = ""
+    email = ""
+    for opt, val in opts:
+        if opt in ('-h', '--help'):
+            usage()
+        elif opt == '--pass':
+            password = val
+        elif opt == '--email':
+            email = val
+
+    if not password:
+        d = Dialog('TurnKey Linux - First boot configuration')
+        password = d.get_password(
+            "ProcessMaker Password",
+            "Enter new password for the ProcessMaker 'admin' account.")
+
+    if not email:
+        if 'd' not in locals():
+            d = Dialog('TurnKey Linux - First boot configuration')
+
+        email = d.get_email(
+            "ProcessMaker Email",
+            "Enter email address for the ProcessMaker 'admin' account.",
+            "admin@example.com")
+
+    hashpass = hashlib.md5(password).hexdigest()
+
+    m = MySQL()
+
+    for database in ('wf_workflow', 'rb_workflow'):
+        m.execute('UPDATE %s.USERS SET USR_PASSWORD=\"%s\" WHERE USR_USERNAME=\"admin\";' % (database, hashpass))
+        m.execute('UPDATE %s.USERS SET USR_EMAIL=\"%s\" WHERE USR_USERNAME=\"admin\";' % (database, email))
+
+
+if __name__ == "__main__":
+    main()
+
